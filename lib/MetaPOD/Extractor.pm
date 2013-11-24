2b19b4f02d20869e9c48c69efe3bcdaa8a60ac42
Kent Fredric <kentfredric@gmail.com>
2013-11-24 16:55:16 +1300

    Build results of a3e59c5 (on master)

--- lib/MetaPOD/Extractor.pm
+++ lib/MetaPOD/Extractor.pm
@@ -6,7 +6,7 @@ BEGIN {
   $MetaPOD::Extractor::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $MetaPOD::Extractor::VERSION = '0.3.1';
+  $MetaPOD::Extractor::VERSION = '0.3.5';
 }
 
 # ABSTRACT: Extract MetaPOD declarations from a file.
@@ -252,7 +252,7 @@ MetaPOD::Extractor - Extract MetaPOD declarations from a file.
 
 =head1 VERSION
 
-version 0.3.1
+version 0.3.5
 
 =head1 METHODS
 
