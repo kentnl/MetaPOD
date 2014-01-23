7d2b8e54cfc7d9d30457461f907076d19d2e5f82
Kent Fredric <kentfredric@gmail.com>
2014-01-24 00:15:11 +1300

    Extractor.pm: make variables slightly lesss ambiguous

--- lib/MetaPOD/Extractor.pm
+++ lib/MetaPOD/Extractor.pm
@@ -197,9 +197,9 @@ sub end_segment {
 =cut
 
 sub append_segment_data {
-  my ( $self, $data ) = @_;
+  my ( $self, $segment_data ) = @_;
   $self->segment_cache->{data} ||= q{};
-  $self->segment_cache->{data} .= $data;
+  $self->segment_cache->{data} .= $segment_data;
   return $self;
 }
 
@@ -210,11 +210,11 @@ sub append_segment_data {
 =cut
 
 sub add_segment {
-  my ( $self, $format, $version, $data, $start_line ) = @_;
+  my ( $self, $format, $version, $section_data, $start_line ) = @_;
   my $segment = {};
   $segment->{format}     = $format;
   $segment->{version}    = $version if defined $version;
-  $segment->{data}       = $data;
+  $segment->{data}       = $section_data;
   $segment->{start_line} = $start_line if defined $start_line;
 
   push @{ $self->segments }, $segment;
